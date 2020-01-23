cca03d889f8eb4922c7609eed462deac7afee042
Shahriar Shanto <shahriar083@yahoo.com>
2020-01-23 14:22:36 +0600

    Add files via upload

--- Run.bat
+++ Run.bat
@@ -1,8 +1,9 @@
 @echo off
-title Shahriar SpeedTest Tool v1.0
-echo Shahriar SpeedTest Tool v1.0
+color a
+title SpeedTest Tool v1.0 ~ Shahriar Shanto
+echo SpeedTest Tool v1.0 ~ Shahriar Shanto
 type art.txt
-echo "1 for Internet | 2 for BDIX"
+echo Enter 1 for Internet // 2 for BDIX
 choice /N /T 5 /C 12 /D 1 /M "Your choice:"
 IF ERRORLEVEL 1 SET id=4235
 IF ERRORLEVEL 2 SET id=14414
