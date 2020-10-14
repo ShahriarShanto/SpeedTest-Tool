7db7a0ab8ddeb9311e3bc9f298eb04f5b541526b
Shahriar Shanto <shahriar083@yahoo.com>
2020-10-14 13:45:56 +0600

    version 1.5

--- PingTest.bat
+++ PingTest.bat
@@ -1,19 +1,22 @@
 @echo off
 color a
-title PingTest Tool
+title SpeedTest Tool v1.5 ~ PingTest Mode
+
 :begin
-echo PingTest Tool
+echo SpeedTest Tool v1.5 ~ PingTest Mode
 echo.
-echo Test #1 : Testing with 1.1.1.1 Cloudflare
-nping -v-1 -icmp --rate 18 -c 72 --data-length 1024 1.1.1.1 | findstr /v Starting | findstr /v Nping
+echo GitHub: https://github.com/ShahriarShanto/SpeedTest-Tool
 echo.
-echo.
-echo Test #2 : Testing with 8.8.8.8 Google
-nping -v-1 -icmp --rate 18 -c 72 --data-length 1024 8.8.8.8 | findstr /v Starting | findstr /v Nping
+echo Test #1 : Sending pings to 1.1.1.1 Cloudflare
+nping -v-1 -icmp --rate 10 -c 35 1.1.1.1 | findstr /v Starting | findstr /v Nping
 echo.
 echo.
-echo Test #3 : Testing with 103.254.152.254 Singapore
-nping -v-1 -icmp --rate 18 -c 72 --data-length 1024 103.254.152.254 | findstr /v Starting | findstr /v Nping
+echo Test #2 : Sending pings to 8.8.8.8 Google
+nping -v-1 -icmp --rate 10 -c 35 8.8.8.8 | findstr /v Starting | findstr /v Nping
+echo.
+echo.
+echo Test #3 : Sending pings to 103.254.152.254 Singapore
+nping -v-1 -icmp --rate 10 -c 35 103.254.152.254 | findstr /v Starting | findstr /v Nping
 echo.
 timeout /T 10
 cls
--- Run.bat
+++ Run.bat
@@ -1,13 +1,53 @@
 @echo off
 color a
-title SpeedTest Tool v1.2 ~ Shahriar Shanto
-echo SpeedTest Tool v1.2 ~ Shahriar Shanto
+title SpeedTest Tool v1.5
+echo SpeedTest Tool v1.5
+echo.
+echo GitHub: https://github.com/ShahriarShanto/SpeedTest-Tool
 type art.txt
-echo Enter 1 for Internet // 2 for BDIX
-choice /N /T 5 /C 12 /D 1 /M "Your choice:"
-IF ERRORLEVEL 1 SET id=4235
-IF ERRORLEVEL 2 SET id=14414
+choice /N /T 5 /C 0123456789 /D 1 /M "Your choice:"
+
+IF %ERRORLEVEL%==10 SET id=25985
+IF %ERRORLEVEL%==9 SET id=15870
+IF %ERRORLEVEL%==8 SET id=15047
+IF %ERRORLEVEL%==7 SET id=20637
+IF %ERRORLEVEL%==6 SET id=2054
+IF %ERRORLEVEL%==5 SET id=367
+IF %ERRORLEVEL%==4 SET id=14367
+IF %ERRORLEVEL%==3 SET id=26528
+IF %ERRORLEVEL%==2 SET id=3366
+IF %ERRORLEVEL%==1 GOTO PingTest
+
 cls
-echo SpeedTest Tool v1.2 ~ Shahriar Shanto
+echo SpeedTest Tool v1.5
+echo.
+echo GitHub: https://github.com/ShahriarShanto/SpeedTest-Tool
 engine -s %id%
+GOTO END
+
+:PingTest
+cls
+title SpeedTest Tool v1.5 ~ PingTest Mode
+
+:begin
+echo SpeedTest Tool v1.5 ~ PingTest Mode
+echo.
+echo GitHub: https://github.com/ShahriarShanto/SpeedTest-Tool
+echo.
+echo Test #1 : Sending pings to 1.1.1.1 Cloudflare
+nping -v-1 -icmp --rate 10 -c 35 1.1.1.1 | findstr /v Starting | findstr /v Nping
+echo.
+echo.
+echo Test #2 : Sending pings to 8.8.8.8 Google
+nping -v-1 -icmp --rate 10 -c 35 8.8.8.8 | findstr /v Starting | findstr /v Nping
+echo.
+echo.
+echo Test #3 : Sending pings to 103.254.152.254 Singapore
+nping -v-1 -icmp --rate 10 -c 35 103.254.152.254 | findstr /v Starting | findstr /v Nping
+echo.
+timeout /T 10
+cls
+goto begin
+
+:END
 PAUSE>nul
